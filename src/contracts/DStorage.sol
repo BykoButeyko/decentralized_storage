49818980114e35a67ff3720e788faa36ce8e906c
Byko <bykocrypto@gmail.com>
2020-11-18 23:59:48 +0100

    fix bugs & make sure hash is there

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -4,6 +4,7 @@ contract DStorage {
   // Name
   string public name = 'DStorage';
   // Number of files
+  uint public fileCount = 0;
   // Mapping fileId=>Struct 
   mapping( uint => File) public files;
 
@@ -27,11 +28,8 @@ contract DStorage {
   // Upload File function
   function  uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
 
-  files[1] = File(1, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
-  }
-
-    // Make sure the file hash exists
-
+  // Make sure the file hash exists
+  require(bytes(_fileHash).length>0);
     // Make sure file type exists
 
     // Make sure file description exists
@@ -42,10 +40,16 @@ contract DStorage {
 
     // Make sure file size is more than 0
 
+  // Increment file id
+  fileCount++;
 
-    // Increment file id
+  // Add File to the contract
+  files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+  };
 
-    // Add File to the contract
+
+
+
 
     // Trigger an event
 
