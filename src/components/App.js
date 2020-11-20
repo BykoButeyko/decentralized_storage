e03a8800e17d4810bb43bb50cdd9d5ab35350afc
Byko <bykocrypto@gmail.com>
2020-11-20 20:54:42 +0100

    setup web3

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,16 @@ class App extends Component {
 
   async loadWeb3() {
     //Setting up Web3
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying Metamask!')
+    }
   }
 
   async loadBlockchainData() {
